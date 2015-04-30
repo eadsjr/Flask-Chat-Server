659fef8e90a445b91ef338905628ad6e3b0a003c
Jason Randolph Eads <jeads442@gmail.com>
2015-04-29 20:54:40 -0700

    websockets now functioning

--- public_html/js/chatserver.js
+++ public_html/js/chatserver.js
@@ -2,5 +2,41 @@
  * Copyright 2015 Jason Randolph Eads - all rights reserved
  */
 
-alert("yo!");
+//alert("yo!");
+console.log("running");
 
+var socket = '';
+
+$(document).ready(function(){
+    console.log("document is ready");
+    namespace = '';
+    
+    socket = io.connect();
+    socket.on('connected', function(msg) {
+        console.log(msg);
+        console.log('Whee!');
+        socket.emit('event1', {data: 'Client connected!'});
+    });
+    socket.on('message', function(msg) {
+        console.log(msg);
+    });
+    socket.on('special', function(msg) {
+        console.log(msg);
+    });
+    socket.on('retrieve-usernames-response', function(data) {
+        console.log(data);
+    });
+    
+    //console.log(socket);
+});
+
+
+function doDebug() {
+    console.log("button pressed");
+    
+    //var socket = io.connect();
+    
+    //socket.emit('event1','button pressed');
+    //socket.emit('event1','button pressed');
+    socket.emit('retrieve-usernames');
+}
--- server/chatserver.py
+++ server/chatserver.py
@@ -1,21 +1,51 @@
 
 # Copyright 2015 Jason Randolph Eads - all rights reserved
 
-
+import json
 from flask import Flask
+from flask.ext.socketio import SocketIO, emit
 
 
-app = Flask("ChatServer")
-
-@app.route('/')
-def hello_world():
-	return 'Hello World'
+app = Flask("ChatServer", static_url_path='', static_folder='../public_html/')
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
 
-@app.route('/user')
-def get_user():
-	return ''
+socketio = SocketIO(app)
+
+#@app.route('/')
+
+#@socketio.on('connect', '/chatserver')
+
+
+
+@socketio.on('retrieve-usernames')
+def retrieve_usernames():
+	print 'usernames requested'
+	usernames = None
+	with open('usernames.json', 'r') as file:
+		usernames = json.loads(file.read())
+		emit('retrieve-usernames-response', {'data':usernames})
+		print 'sent usernames'
+
+
+@socketio.on('connect')
+def connect():
+	emit('connected', {'data':'Connected to ChatServer'})
+	print 'connected to client'
+
+@socketio.on('disconnect')
+def disconnect():
+	print 'disconnected from client'
+
+
+@socketio.on('event1')
+def handle_event(message):
+	print 'recieved message "%s"' % message
+
+#@socketio.on('')
 
 
 if __name__ == '__main__':
-	app.run()
+	#app.run()
+	#socketio.run(app, port=int(os.environ.get("PORT",9001)))
+	socketio.run(app)
