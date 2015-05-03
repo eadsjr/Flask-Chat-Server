717086327e78e3e0f03ec44c14c73f876a81d43e
Jason Randolph Eads <jeads442@gmail.com>
2015-05-03 08:47:00 -0700

    Dropdown now populates and responds to events

--- public_html/js/chatserver.js
+++ public_html/js/chatserver.js
@@ -1,43 +1,62 @@
-/* 
+/*
  * Copyright 2015 Jason Randolph Eads - all rights reserved
  */
 
-//alert("yo!");
 console.log("running");
 
 // Communicate with backend via websocket
 var socket = '';
 $(document).ready(function(){
-    console.log("document is ready");
-    namespace = '';
-    
-    socket = io.connect();
-    socket.on('connected', function(msg) {
-        console.log(msg);
-        console.log('Whee!');
-        socket.emit('event1', {data: 'Client connected!'});
-    });
-    socket.on('message', function(msg) {
-        console.log(msg);
-    });
-    socket.on('special', function(msg) {
-        console.log(msg);
-    });
-    socket.on('retrieve-usernames-response', function(data) {
-        console.log(data);
-    });
-    
-    //console.log(socket);
-});
+				  console.log("document is ready");
+				  namespace = '';
+				  
+				  socket = io.connect();
+				  socket.on('connected', function(msg) {
+							console.log(msg);
+							console.log('Whee!');
+							socket.emit('event1', {data: 'Client connected!'});
+							});
+				  socket.on('message', function(msg) {
+							console.log(msg);
+							});
+				  socket.on('special', function(msg) {
+							console.log(msg);
+							});
+				  socket.on('retrieve-usernames-response', function(json) {
+							updateUsersDropdown(json.data);
+							});
+				  
 
+				  // Add event handler for dropdown
+				  $("#start-conversation-select").on( "change", function(event) {startConversation(event.currentTarget.value)} );
+				  
+				  });
+
+
+// Create a new conversation window
+function startConversation(username) {
+	if(username == "") {
+		return;
+	}
+	console.log("conversation with %s started", username);
+}
+
+
+// unpacks usernames and updates the select box
+function updateUsersDropdown(data) {
+	var optionlist = "<option></option>";
+	for( elem in data ) {
+		optionlist += "<option>" + data[elem].username + "</option>";
+	}
+	$("#start-conversation-select").html(optionlist);
+}
 
 function doDebug() {
-    console.log("button pressed");
-    
-    //var socket = io.connect();
-    
-    //socket.emit('event1','button pressed');
-    //socket.emit('event1','button pressed');
-    socket.emit('retrieve-usernames');
+	console.log("button pressed");
+	
+	//var socket = io.connect();
+	
+	//socket.emit('event1','button pressed');
+	socket.emit('retrieve-usernames');
 }
 
